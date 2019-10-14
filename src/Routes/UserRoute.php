1b8b1165a5aaadca424a29a1954c68b1e01a5015
Tycho van Berge Henegouwen <info@bespokeweb.nl>
2019-10-14 17:14:08 +0200

    fix password reset link

--- src/Routes/UserRoute.php
+++ src/Routes/UserRoute.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Tychovbh\Mvc\Routes;
 
-use Tychovbh\Mvc\Http\Controllers\UserController;
-
 class UserRoute extends AbstractRoutes implements Routes
 {
     /**
@@ -19,12 +17,7 @@ class UserRoute extends AbstractRoutes implements Routes
         $instance->show('users', $options);
         $instance->store('users', $options, ['validate']);
         $instance->route('post', 'users.login', 'login', '/users/login', $options);
-        $instance->update('users.password_reset', array_merge([
-            'update' => [
-                'uses' => UserController::class . '@resetPassword',
-                'url' => '/users/password_reset',
-            ]
-        ], $options), ['validate']);
+        $instance->route('put', 'users.password_reset', 'resetPassword', '/users/password_reset', $options, ['validate']);
         $instance->update('users', $options);
         $instance->destroy('users', $options);
     }
