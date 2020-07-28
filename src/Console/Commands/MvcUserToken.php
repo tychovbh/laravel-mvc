0f454c3a40fc49f2d598e6bfda9f4797979ed693
Tycho van Berge Henegouwen <info@bespokeweb.nl>
2020-07-28 12:08:34 +0200

    improve token generation

--- src/Console/Commands/MvcUserToken.php
+++ src/Console/Commands/MvcUserToken.php
@@ -43,14 +43,22 @@ class MvcUserToken extends Command
         $email = $this->option('email') ?? $this->ask('For which email do you want to generate a token?');
         try {
             $user = $repository->findBy('email', $email);
-            $expiration = $this->option('expiration') ?? $this->ask('When should the token expire? YYYY-MM-DD HH:MM:SS');
-            $expiration = Carbon::createFromFormat('Y-m-d H:i:s', $expiration)->timestamp;
+            $type = $this->option('type') ?? $this->choice('What type of token?', [
+                    TokenType::API_KEY,
+                    TokenType::USER_TOKEN
+                ]);
+            $expiration = null;
+            if ($this !== TokenType::API_KEY) {
+                $expiration = $this->option('expiration') ?? $this->ask('When should the token expire? YYYY-MM-DD HH:MM:SS');
+                $expiration = Carbon::createFromFormat('Y-m-d H:i:s', $expiration)->timestamp;
+            }
 
             $this->info('Token: ' . token([
                     'id' => $user->id,
-                    'type' => $this->option('type') ?? $this->choice('What type of token?', [TokenType::API_KEY, TokenType::USER_TOKEN])
+                    'type' => $type
                 ], $expiration)
             );
+            
         } catch (\Exception $exception) {
             $this->error($exception->getMessage());
         }
