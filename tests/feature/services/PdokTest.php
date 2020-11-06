6ad5a09fb7d83c5a51261353d3916ee6910ab034
Thoompje123 <thvrijn2002@gmail.com>
2020-11-06 09:57:03 +0100

    Made PdokTest + AddressLookupInterface

---
+++ tests/feature/services/PdokTest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Tychovbh\Tests\Mvc\feature\services;
+
+
+use GuzzleHttp\Exception\GuzzleException;
+use Tychovbh\Mvc\Services\AddressLookup\PdokService;
+use Tychovbh\Tests\Mvc\TestCase;
+
+class PdokTest extends TestCase
+{
+    /**
+     * @test
+     * @throws GuzzleException
+     */
+    public function itCanSearch()
+    {
+        $search = PdokService::search('2352 CZ', '38');
+        $this->assertEquals('Touwbaan', $search['street']);
+    }
+}
