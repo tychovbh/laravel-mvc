3205ac7ec14886ebf8b1aeddcf9499ec92ec05b8
Tycho van Berge Henegouwen <info@bespokeweb.nl>
2020-12-04 10:59:09 +0100

    replace testhelper resource for  json resource

--- src/Tests/Lumen/TestHelper.php
+++ src/Tests/Lumen/TestHelper.php
@@ -6,7 +6,7 @@ namespace Tychovbh\Mvc\Tests\Lumen;
 use App\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
-use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
@@ -75,11 +75,11 @@ trait TestHelper
 
     /**
      * See resource
-     * @param Resource $resource
+     * @param JsonResource $resource
      * @param array $except
      * @return TestCase
      */
-    public function seeResource(Resource $resource, array $except = [])
+    public function seeResource(JsonResource $resource, array $except = [])
     {
         return $this->seeJsonExcept($resource->response($this->app['request'])->getData(true), null, $except);
     }
@@ -236,7 +236,7 @@ trait TestHelper
 
     /**
      * Perform show request
-     * @param Resource $resource
+     * @param JsonResource $resource
      * @param array $params
      * @param bool $token
      * @param array $tokenParams
@@ -244,7 +244,7 @@ trait TestHelper
      * @return array
      */
     public function show(
-        Resource $resource,
+        JsonResource $resource,
         array $params = [],
         bool $token = false,
         array $tokenParams = [],
@@ -280,13 +280,13 @@ trait TestHelper
 
     /**
      * Perform store request
-     * @param Resource $resource
+     * @param JsonResource $resource
      * @param array $params
      * @param array $tokenParams
      * @param array $except
      * @return array
      */
-    public function store(Resource $resource, array $params = [], array $tokenParams = [], array $except = []): array
+    public function store(JsonResource $resource, array $params = [], array $tokenParams = [], array $except = []): array
     {
         $request = $this->post(route($this->indexName() . '.store'), $params, $this->token($tokenParams))
             ->seeResource($resource, $except)
@@ -310,13 +310,13 @@ trait TestHelper
 
     /**
      * Perform update request
-     * @param Resource $resource
+     * @param JsonResource $resource
      * @param array $params
      * @param array $tokenParams
      * @param array $except
      * @return array
      */
-    public function update(Resource $resource, array $params = [], array $tokenParams = [], array $except = []): array
+    public function update(JsonResource $resource, array $params = [], array $tokenParams = [], array $except = []): array
     {
         $request = $this->put(route($this->indexName() . '.update', [
             'id' => $resource->id
