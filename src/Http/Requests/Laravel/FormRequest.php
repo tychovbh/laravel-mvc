d7626cfc13f51b1641e9f8b7407af8d5329ca174
Tycho van Berge Henegouwen <info@bespokeweb.nl>
2021-05-02 22:52:47 +0200

    fix laravel form request

--- src/Http/Requests/Laravel/FormRequest.php
+++ src/Http/Requests/Laravel/FormRequest.php
@@ -2,10 +2,31 @@
 
 namespace Tychovbh\Mvc\Http\Requests\Laravel;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest as BaseFormRequest;
+use Illuminate\Validation\ValidationException;
 use Tychovbh\Mvc\Http\Requests\RequestSettings;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 abstract class FormRequest extends BaseFormRequest
 {
     use RequestSettings;
+
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  Validator  $validator
+     * @return void
+     * @throws HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+
+        throw new HttpResponseException(
+            response()->json(['errors' => $errors], JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
 }
